a279538dee2f4f2ae66c885b80016b3adf53a298
Vishal Kumar Maurya <vm7740014@gmail.com>
2023-06-01 13:07:25 +0530

    admin dashboard info card design complete

--- frontend/src/pages/Admin/AdminDashboard.jsx
+++ frontend/src/pages/Admin/AdminDashboard.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import {AiOutlineHome} from 'react-icons/ai'
+import { AiOutlineHome,AiFillPlayCircle } from "react-icons/ai";
+import { FaUsers,FaRupeeSign } from "react-icons/fa";
+import { VscPassFilled } from "react-icons/vsc";
 const AdminDashboard = () => {
   return (
     <>
@@ -11,10 +13,10 @@ const AdminDashboard = () => {
               <img
                 className="w-7 h-7 md:w-10 md:h-10 mr-2 rounded-md overflow-hidden"
                 src="https://therminic2018.eu/wp-content/uploads/2018/07/dummy-avatar.jpg"
-                alt=''
+                alt=""
               />
               <span className="hidden md:block ">ADMIN</span>
-            </div>  
+            </div>
           </div>
           <div className="fixed flex flex-col top-14 left-0 w-14 hover:w-64 md:w-64 bg-[#152C4F] h-full text-white transition-all duration-300 border-none z-10 sidebar">
             <div className="overflow-y-auto overflow-x-hidden flex flex-col justify-between flex-grow">
@@ -22,16 +24,17 @@ const AdminDashboard = () => {
                 <li className="px-5 hidden md:block">
                   <div className="flex flex-row items-center h-8">
                     <div className="text-sm font-light tracking-wide text-gray-400 uppercase">
-                     Quiz 
+                      Quiz
                     </div>
                   </div>
                 </li>
                 <li>
                   <NavLink
+                    to="/quiz"
                     className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-blue-100   hover:text-black border-l-4 border-transparent  pr-6"
                   >
                     <span className="inline-flex justify-center items-center ml-4">
-                    <AiOutlineHome/>
+                      <AiOutlineHome />
                     </span>
                     <span className="ml-2 text-sm tracking-wide truncate">
                       Quiz Questions
@@ -40,10 +43,11 @@ const AdminDashboard = () => {
                 </li>
                 <li>
                   <NavLink
+                    to="/createQuestion"
                     className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-blue-100   hover:text-black border-l-4 border-transparent  pr-6"
                   >
                     <span className="inline-flex justify-center items-center ml-4">
-                    <AiOutlineHome/>
+                      <AiOutlineHome />
                     </span>
                     <span className="ml-2 text-sm tracking-wide truncate">
                       Create Questions
@@ -52,10 +56,11 @@ const AdminDashboard = () => {
                 </li>
                 <li>
                   <NavLink
+                    to="/updateQuestion"
                     className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-blue-100   hover:text-black border-l-4 border-transparent  pr-6"
                   >
                     <span className="inline-flex justify-center items-center ml-4">
-                    <AiOutlineHome/>
+                      <AiOutlineHome />
                     </span>
                     <span className="ml-2 text-sm tracking-wide truncate">
                       Update Question
@@ -70,11 +75,9 @@ const AdminDashboard = () => {
                   </div>
                 </li>
                 <li>
-                  <NavLink
-                    className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-blue-100   hover:text-black border-l-4 border-transparent  pr-6"
-                  >
+                  <NavLink className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-blue-100   hover:text-black border-l-4 border-transparent  pr-6">
                     <span className="inline-flex justify-center items-center ml-4">
-                    <AiOutlineHome/>
+                      <AiOutlineHome />
                     </span>
                     <span className="ml-2 text-sm tracking-wide truncate">
                       User List
@@ -85,11 +88,9 @@ const AdminDashboard = () => {
                   </NavLink>
                 </li>
                 <li>
-                  <NavLink
-                    className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-blue-100   hover:text-black border-l-4 border-transparent  pr-6"
-                  >
+                  <NavLink className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-blue-100   hover:text-black border-l-4 border-transparent  pr-6">
                     <span className="inline-flex justify-center items-center ml-4">
-                    <AiOutlineHome/>
+                      <AiOutlineHome />
                     </span>
                     <span className="ml-2 text-sm tracking-wide truncate">
                       Users Result
@@ -104,11 +105,9 @@ const AdminDashboard = () => {
                   </div>
                 </li>
                 <li>
-                  <NavLink
-                    className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-blue-100   hover:text-black border-l-4 border-transparent  pr-6"
-                  >
+                  <NavLink className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-blue-100   hover:text-black border-l-4 border-transparent  pr-6">
                     <span className="inline-flex justify-center items-center ml-4">
-                    <AiOutlineHome/>
+                      <AiOutlineHome />
                     </span>
                     <span className="ml-2 text-sm tracking-wide truncate">
                       Profile
@@ -116,11 +115,9 @@ const AdminDashboard = () => {
                   </NavLink>
                 </li>
                 <li>
-                  <NavLink
-                    className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-blue-100   hover:text-black border-l-4 border-transparent  pr-6"
-                  >
+                  <NavLink className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-blue-100   hover:text-black border-l-4 border-transparent  pr-6">
                     <span className="inline-flex justify-center items-center ml-4">
-                    <AiOutlineHome/>
+                      <AiOutlineHome />
                     </span>
                     <span className="ml-2 text-sm tracking-wide truncate">
                       Settings
@@ -134,7 +131,46 @@ const AdminDashboard = () => {
             </div>
           </div>
 
-        
+          <div class="h-full ml-14 mt-2 mb-10 md:ml-64">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 p-4 gap-4 h-44">
+              <div className="bg-[#152C4F]  shadow-lg rounded-md flex items-center justify-between p-3  text-white font-medium group hover:bg-blue-100 hover:text-black">
+                <div className="flex justify-center items-center w-16 h-16 bg-white rounded-full transition-all duration-300 transform group-hover:-rotate-12 text-[#152C4F] hover:bg-blue-200 hover:text-black">
+                    <FaUsers size={60}/>
+                </div>
+                <div className="text-right">
+                  <p className="text-2xl font-bold">257</p>
+                  <p>Daily Registration</p>
+                </div>
+              </div>
+              <div className="bg-[#152C4F]  shadow-lg rounded-md flex items-center justify-between p-3  text-white font-medium group hover:bg-blue-100 hover:text-black">
+                <div className="flex justify-center items-center w-16 h-16 bg-white rounded-full transition-all duration-300 transform group-hover:-rotate-12 text-[#152C4F] hover:bg-blue-200 hover:text-black">
+                <AiFillPlayCircle size={70}/>
+                </div>
+                <div className="text-right">
+                  <p className="text-2xl font-bold">157</p>
+                  <p>Daily Quiz Played</p>
+                </div>
+              </div>
+              <div className="bg-[#152C4F]  shadow-lg rounded-md flex items-center justify-between p-3  text-white font-medium group hover:bg-blue-100 hover:text-black">
+                <div className="flex justify-center items-center w-16 h-16 bg-white rounded-full transition-all duration-300 transform group-hover:-rotate-12 text-[#152C4F] hover:bg-blue-200 hover:text-black">
+                <VscPassFilled size={70}/>
+                </div>
+                <div className="text-right">
+                  <p className="text-2xl font-bold">121</p>
+                  <p>Daily Passed User</p>
+                </div>
+              </div>
+              <div className="bg-[#152C4F]  shadow-lg rounded-md flex items-center justify-between p-3  text-white font-medium group hover:bg-blue-100 hover:text-black">
+                <div className="flex justify-center items-center w-16 h-16 bg-white rounded-full transition-all duration-300 transform group-hover:-rotate-12 text-[#152C4F] hover:bg-blue-200 hover:text-black">
+                <FaRupeeSign size={50}/>
+                </div>
+                <div className="text-right">
+                  <p className="text-2xl font-bold">₹ 75,257</p>
+                  <p>Daily Earnings</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
