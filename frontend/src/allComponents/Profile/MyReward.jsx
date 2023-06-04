2342d1c4d6755c06f8e11dc5c444268dd51d99a8
Vishal Kumar Maurya <vm7740014@gmail.com>
2023-06-04 13:27:19 +0530

    my reward all bug fixed

--- frontend/src/allComponents/Profile/MyReward.jsx
+++ frontend/src/allComponents/Profile/MyReward.jsx
@@ -1,11 +1,54 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Menu from "./Menu";
+import { axiosInstance } from "../../utils/axiosSetup";
 
 const MyReward = () => {
-  const [rewards,setRewards]=useState()
-  let data = JSON.parse(localStorage.getItem("user"));
-  
-  setRewards(data.rewards )
+  const [rewards, setRewards] = useState();
+  let [totalRewards, setTotalRewards] = useState(0);
+  let token = JSON.parse(localStorage.getItem("token"));
+
+  const emptyData = [
+    {
+      rewardType: "no-referral",
+      date: "nodataTa",
+      code: "nodata to show",
+      data:'empty'
+    },
+  ];
+
+  const config = {
+    headers: {
+      Authorization: token,
+    },
+  };
+  const fetchData = async () => {
+    try {
+      const res = await axiosInstance.get(`/user`, config);
+      if (res.data.success) {
+        if (res.data.data.rewards) {
+          setRewards(res.data.data.rewards);
+          totalRewardsCount(res.data.data.rewards);
+        } else {
+          setRewards(emptyData);
+          totalRewardsCount(emptyData);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const totalRewardsCount = (rewards) => {
+    rewards.map((e) =>
+      setTotalRewards(
+        (e.rewardType = "referral"
+          ? (totalRewards = totalRewards + 500)
+          : (totalRewards = totalRewards + 0))
+      )
+    );
+  };
+  useEffect(() => fetchData, []);
+  console.log(rewards,emptyData);
   return (
     <>
       <div className="bg-[#152C4F] h-44 w-full  mb-5   "></div>
@@ -13,25 +56,36 @@ const MyReward = () => {
         <div className="flex-col">
           <Menu />
         </div>
-        <div className="w-44 h-44 rounded-lg overflow-hidden shadow-lg ">
-            <div className="px-5 text-green-400 text-[2rem]">2333</div>
+        <div className="flex-col">
+          <div className="w-44 h-44 rounded-lg overflow-hidden shadow-lg ">
+            <div className="px-5 text-green-400 text-[2rem]">
+              {totalRewards}
+            </div>
             <div className="px-10">Points</div>
           </div>
-     {     rewards?.map((e)=>(
-        <div className="w-3/5 h-20 rounded-lg overflow-hidden shadow-lg flex">
-          <div className="px-6 mx-10 py-4 flex-col text-[0.8rem]">
-            <div className="  mb-2">{e.data}</div>
-            <div className="  mb-2">{e.date}</div>
+          <div className="flex-col">
+            {rewards?.map((e) => (
+              <div className="w-[100%] h-20 rounded-lg overflow-hidden shadow-lg flex">
+                <div className="px-6 mx-10 py-4 flex-col text-[0.8rem]">
+                  <div className="  mb-2">{e.data}</div>
+                  <div className="  mb-2">{e.date.split("T")[0]}</div>
+                </div>
+                <div className="px-6 mx-10 py-4 text-green-400">
+                  <div className="font-bold text-sm mb-2">
+                    {(e.rewardType = "referral" ? "500" : "")}
+                  </div>
+                </div>
+                <div className="px-6 mx-10 py-4">
+                  <div className=" text-sm mb-2">Gift Voucher: </div>
+                </div>
+                <div className="px-6 mx-1 py-4 bg-gray-300 h-9 w-32">
+                  {" "}
+                  {e.code}
+                </div>
+              </div>
+            ))}
           </div>
-          <div className="px-6 mx-10 py-4 text-green-400">
-            <div className="font-bold text-sm mb-2">500</div>
-          </div>
-          <div className="px-6 mx-10 py-4">
-            <div className=" text-sm mb-2">Gift Voucher:   </div>
-          </div>
-          <div className="px-6 mx-1 py-4 bg-gray-300 h-9 w-32"> {e.code}</div> 
         </div>
-        ))}
       </div>
     </>
   );
