947405248bfd32a4fae0ed8a4ce37669bcb95b92
Vishal Kumar Maurya <vm7740014@gmail.com>
2023-06-05 14:55:10 +0530

    score card detailed view navigation done

--- frontend/src/pages/ScoreCard.jsx
+++ frontend/src/pages/ScoreCard.jsx
@@ -1,14 +1,15 @@
 import React,{useState} from 'react'
-import { useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 const ScoreCard = () => {
 // const[score,setScore]=useState()
+const navigate=useNavigate()
   const [searchParams] = useSearchParams();
   console.log(searchParams.get('score'))
   let data = JSON.parse(localStorage.getItem("user"));
   return (
     <>
-    <div>   
+    <div className=''>   
       <div className="absolute left-[8rem] text-center bg-blue-400 h-72 w-[80%] text-white my-10 rounded">
 
         <div className=" m-5 text-center text-[2rem] flex-col">
@@ -21,7 +22,12 @@ const ScoreCard = () => {
       <div className="flex mx-6 justify-center">
        
       </div> 
-      
+            <button
+        class="bg-transparent hover:bg-[#3D5890] text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded  my-10 mx-1 absolute top-[80%] left-[45%]"
+        onClick={()=>navigate('/myQuiz')}
+      >
+       Detailed View 
+      </button>
       </div>
 
     </>
