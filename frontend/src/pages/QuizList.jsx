31cf2b5ce12694a0d46d12c2d8b708cbd7e1e7d8
Vishal Kumar Maurya <vm7740014@gmail.com>
2023-06-07 14:39:02 +0530

    quiz list responsive

--- frontend/src/pages/QuizList.jsx
+++ frontend/src/pages/QuizList.jsx
@@ -36,7 +36,7 @@ const QuizList = () => {
       <div className="relative top-[6rem] text-[2rem] font-bold text-black text-center ">
         Select the Quiz to Solve
       </div>
-      <div className="flex flex-wrap flex-grow">
+      <div className="flex flex-wrap flex-grow justify-center">
       {category?.map((p) => (
         <div className="relative top-[8rem] px-8 ">
           <button class="bg-[#3D5890] hover:bg-transparent text-white font-semibold hover:text-blue-700 py-2 px-4 border border-blue-500 hover:border-blue-600 rounded-lg shadow-2xl  my-8 mx-5 h-52 w-96 max-[640px]:w-40 max-[640px]:h-16 max-[640px]:mx-2 max-[640px]:my-4" 
