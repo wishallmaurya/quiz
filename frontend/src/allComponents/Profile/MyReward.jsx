7778ec11f1bd27915bed41b539205773b1d68aeb
Vishal Kumar Maurya <vm7740014@gmail.com>
2023-06-09 12:20:23 +0530

    my reward page is responsive

--- frontend/src/allComponents/Profile/MyReward.jsx
+++ frontend/src/allComponents/Profile/MyReward.jsx
@@ -58,36 +58,49 @@ const MyReward = () => {
         <div className="">
           <Menu />
         </div>
-          <div className="w-44 h-44 rounded-lg overflow-hidden shadow-lg ">
-            <div className="px-5 text-green-400 text-[2rem]">
+        <div className="w-auto flex-col justify-center items-center m-2 ">
+          <div className="w-auto  h-auto py-10 border-gray-400 border-2 rounded-lg flex justify-center items-center px-5 max-[640px]:flex-col ">
+            
+            <img
+                className="w-8 h-8 md:w-20 md:h-20 bg-blue-200  rounded-lg"
+                src='https://ik.imagekit.io/wish/MAD_QUIZ_PROJECT/Reward-PNG-Transparent-Image.png?updatedAt=1686288893823'
+                alt=""
+              />
+            <div className="px-5 text-green-600 font-bold text-[2rem]">
               {totalRewards}
             </div>
-            <div className="px-10">Points</div>
+            <div className="text-[0.7rem] pt-3">Points</div>
+            <button class="bg-white hover:bg-[#3D5890] text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded w-auto ml-10" >
+            Redeem Voucher
+          </button>
           </div>
-        <div className="flex flex-col justify-center items-center border-blue-400 border-4">
+        <div className="w-auto mt-10 flex flex-col justify-center items-center">
           <div className="flex-col ">
             {rewards?.map((e) => (
-              <div className="w-[100%] h-20 rounded-lg overflow-hidden shadow-lg flex bor">
-                <div className="px-6 mx-10 py-4 flex-col text-[0.8rem]">
+              <div className="w-[100%] mt-4 h-20 rounded-lg overflow-hidden shadow-lg flex border-gray-400 border-2">
+                <div className="px-6 max-[1080px]:mx-1 max-[480px]:px-1 mx-10 py-4 flex-col text-[0.8rem]">
                   <div className="  mb-2">{e.data}</div>
                   <div className="  mb-2">{e.date.split("T")[0]}</div>
                 </div>
-                <div className="px-6 mx-10 py-4 text-green-400">
+                <div className="px-6 max-[1080px]:mx-1 max-[480px]:px-1 mx-10 py-4 text-green-600 flex">
                   <div className="font-bold text-sm mb-2">
                     {(e.rewardType = "referral" ? "500" : "")}
                   </div>
+                  <div className="pl-2 text-sm mb-2 text-black">
+                    Points
+                  </div>
                 </div>
-                <div className="px-6 mx-10 py-4">
+                <div className="px-6 max-[1080px]:mx-1 max-[480px]:px-1 mx-10 py-4">
                   <div className=" text-sm mb-2">Gift Voucher: </div>
                 </div>
-                <div className="px-6 mx-1 py-4 bg-gray-300 h-9 w-32">
-                  {" "}
+                <div className="p-6 max-[1080px]:mx-1 max-[480px]:px-1 mx-1 py-4 bg-gray-300 h-9 w-32 border-2 border-gray-400 border-dashed">
                   {e.code}
                 </div>
               </div>
             ))}
           </div>
         </div>
+      </div>
       </div>
     </>
   );
